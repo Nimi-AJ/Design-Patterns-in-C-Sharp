c2c01e8900436e7e5d69a955d97e2ea3c54000a7
Nimi-AJ <toluwanimi.ajike@stu.cu.edu.ng>
2023-04-03 11:25:32 +0100

    Life Without Builder Pattern

---
+++ DesignPatterns/Builder Pattern/LifeWithoutBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace DesignPatterns.BuilderPattern
+{
+    public class LifeWithoutBuilder
+    {
+        public LifeWithoutBuilder()
+        {
+        }
+
+        public static void main()
+        {
+            var hello = "world";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+        }
+    }
+}
+
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using DesignPatterns.BuilderPattern;
+
+namespace DesignPatterns
+{
+    public class Demo1
+    {
+        //public Demo()
+        //{
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            LifeWithoutBuilder.main();
+            Console.ReadKey();
+        }
+    }
+}
